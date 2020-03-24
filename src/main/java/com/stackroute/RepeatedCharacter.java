6d430b283de2692baedb9130229594c843e3e232
ubuntu <ubuntu@ip-172-31-95-151.ec2.internal>
2020-03-24 11:17:43 +0000

    initial commit

--- src/main/java/com/stackroute/RepeatedCharacter.java
+++ src/main/java/com/stackroute/RepeatedCharacter.java
@@ -1,14 +1,44 @@
 package com.stackroute;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class RepeatedCharacter {
 
-   	//Do not print anything other than what's expected/asked in problem
-    public static void main(String[] args) {
-   	//Use Scanner to get input from console
+	// Do not print anything other than what's expected/asked in problem
+	public static void main(String[] args) {
+		// Use Scanner to get input from console
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		System.out.println(findRepeatedCharacter(input));
 
 	}
 
 	public static Character findRepeatedCharacter(String input) {
-		return '1';
+		if (input == null || input.isBlank())
+			return '0';
+		char[] array = input.toCharArray();
+		Arrays.sort(array);
+		int count = 1;
+		int max = 0;
+		char maxChar = 0;
+		for (int i = 1; i < array.length; i++) {
+			if (array[i] == array[i - 1]) {
+				count++;
+			} else {
+				if (count > max) {
+					max = count;
+					maxChar = array[i - 1];
+
+				}
+				count = 1;
+			}
+		}
+		if (count > max) {
+			max = count;
+			maxChar = array[array.length - 1];
+		}
+		return maxChar;
+
 	}
 }
